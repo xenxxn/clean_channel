8cd710cf1bc7d14d509c998426b64ce375e9ce69
zenxxn <bunbun2438@naver.com>
2024-01-02 16:47:33 +0900

    Add: 회원가입 request dto에 비밀번호 패턴 추가 및 변수명 변경

--- src/main/java/com/zero_channel/member/data/dto/SignUpRequest.java
+++ src/main/java/com/zero_channel/member/data/dto/SignUpRequest.java
@@ -3,6 +3,7 @@ package com.zero_channel.member.data.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,15 +24,17 @@ public class SignUpRequest {
   @Schema(description = "이메일", example = "zero@naver.com")
   private String email;
 
-  @Size(min = 12, message = "비밀번호는 최소 12글자 이상 입력해주시길 바랍니다.")
+  @Size(min = 8, message = "비밀번호는 최소 8글자 이상 입력해주시길 바랍니다.")
   @NotBlank
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,30}$",
+      message = "비밀번호는 8~30 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
   @Schema(description = "비밀번호", example = "zeroChannelPassword")
   private String password;
 
   @Size(min = 12, message = "비밀번호는 최소 12글자 이상 입력해주시길 바랍니다.")
   @NotBlank
   @Schema(description = "비밀번호 확인", example = "zeroChannelPassword")
-  private String rePassword;
+  private String checkedPassword;
 
   @NotBlank(message = "이름은 필수 항목입니다.")
   @Schema(description = "이름", example = "김제로")
